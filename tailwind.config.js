540a93d7ad8179b068aeceab37d7eeeefe5bdfa0
Sammy949 <urahsamuel0202@gmail.com>
2023-08-10 11:54:05 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}*"],
+  theme: {
+    screens: {
+      sm: '590px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
